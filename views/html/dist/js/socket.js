9139fee656c0881631ecf97f9e703268136fec5c
Oladele Olafimihan <delafsolutions@gmail.com>
2021-03-12 11:04:56 +0100

    Edited socket.js

--- views/html/dist/js/socket.js
+++ views/html/dist/js/socket.js
@@ -1,5 +1,17 @@
 
 
+var data_socket_transporter = io.connect("http://139.162.192.74:2021");
+    
+console.log(data_socket_transporter);
+
+data_socket_transporter.on('errmsg', (errmsg) => {
+    console.log(errmsg);
+});
+
+// document.ready(function(){
+//     alert('Dele')
+// })
+
 
 // var data_socket_transporter = io.connect("http://127.0.0.1:2000");
 // var data_socket_transporter = io.connect("http://139.162.229.38:2000");
