ef098a5093289f88f817839669164bcc41af055d
Oladele Olafimihan <delafsolutions@gmail.com>
2021-03-12 11:01:29 +0100

    Added new files

---
+++ views/html/dist/js/socket.js
@@ -0,0 +1,11 @@
+
+
+
+// var data_socket_transporter = io.connect("http://127.0.0.1:2000");
+// var data_socket_transporter = io.connect("http://139.162.229.38:2000");
+    
+// console.log(data_socket_transporter);
+
+// document.ready(function(){
+//     alert('Dele')
+// })
