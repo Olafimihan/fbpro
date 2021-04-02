46eaa1f6456c5fb2392417f911611d16e05758f0
Oladele Olafimihan <delafsolutions@gmail.com>
2021-04-02 20:35:37 +0100

    Initial Datatable data sources updates

--- testserver.js
+++ testserver.js
@@ -43,8 +43,8 @@ let outResultArray = [];
 // ================================================================
 // setup our express application
 // ================================================================
-// app.set('view engine', 'html');
-// app.use('public', express.static(process.cwd() + 'public'));
+app.set('view engine', 'html');
+app.use('public', express.static(process.cwd() + 'public'));
 // app.engine('html', require('ejs').renderFile);
 
 app.set('views', __dirname);
@@ -61,7 +61,7 @@ let storage = multer.diskStorage({
      }
 });
 
- 
+ console.log(storage)
 
  
    
